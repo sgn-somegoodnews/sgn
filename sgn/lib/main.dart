9f09b48bf56d5df6456d9ada80e78ca2b82497a6
danihmello06 <48775164+danihmello06@users.noreply.github.com>
2020-04-29 12:13:08 -0300

    feature: add routes (#4)
    Co-authored-by: Daniela <daniela.mello@dextra-sw.com>

--- sgn/lib/main.dart
+++ sgn/lib/main.dart
@@ -1,24 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:sgn/screens/home_screen/home_screen.dart';
-import 'package:sgn/theme.dart';
 
-import 'stores/news_store.dart';
 
 void main() {
-  runApp(
-    MultiProvider(
-      providers: [Provider(create: (_) => new NewsFeedStore())],
-      child: SgnApp(),
-    ),
-  );
+  runApp(SgnApp());
 }
 
 class SgnApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        title: "SGN - Some good news",
-        theme: theme,
-        home: HomeScreen(),
-      );
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomeScreen(),
+      },
+    );
+
+  }
 }
+
