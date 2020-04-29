241f6fabe2f2c7ac20cebbc9ee8b5af8e0c47c79
renancaraujo <renan.araujo@dashdash.com>
2020-04-29 21:13:46 +0100

    native files

--- sgn/lib/screens/news_details.dart
+++ sgn/lib/screens/news_details.dart
@@ -143,18 +143,6 @@ class NewsDetailsHeaderDelegate implements SliverPersistentHeaderDelegate {
     );
   }
 
-  Color _backgroundColor(double shrinkOffset) {
-    final percExpanded = _getPercExpandedUntilZero(shrinkOffset);
-    final transparency = percExpanded / percExpandedStartOpaquingImg;
-    if (transparency > 1) return Colors.transparent;
-    return endBackgroundColor.withOpacity(1.0 - transparency);
-  }
-
-  double _titleSize(double shrinkOffset,
-      {@required double maxSize, @required double minSize}) {
-    return minSize + _getPercExpanded(shrinkOffset) * (maxSize - minSize);
-  }
-
   double _getPercExpanded(double shrinkOffset) {
     return 1.0 - min(maxExtent - minExtent, shrinkOffset) / maxExtent;
   }
@@ -198,7 +186,7 @@ class NewsDetailsContent extends StatelessWidget {
                   style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                 )),
             Text(
-              "${news.text} \n ${_mockDescription()}",
+              "${news.text}",
               style: TextStyle(
                   fontSize: 16, height: 1.4, color: Color(0xFF454545)),
             ),
@@ -207,14 +195,5 @@ class NewsDetailsContent extends StatelessWidget {
         ),
       ),
     );
-  }
-
-  // TODO: remove this
-  String _mockDescription() {
-    return """
-    At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
-    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    """;
   }
 }
---
+++ sgn/lib/screens/story_screen/story_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class StoryScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
+
+class StoryPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
